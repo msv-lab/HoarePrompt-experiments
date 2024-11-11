492a416a1e05601e511fe27681cbf86c7ac9c3fe
jimbou <jimbou1999@gmail.com>
2024-11-11 11:29:22 +0800

    run pilot experiments

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-./test.sh apps 2
-./test.sh apps 3
-./test.sh apps 1
-./test.sh mbpp 2
-./test.sh mbpp 3
-./test.sh mbpp 1
+./test.sh apps 2 default_config_4_mini.json # done ektos apo  missing
+# ./test.sh apps 1 default_config_llama.json
+./test.sh apps 2 default_config.json
+./test.sh apps 3 default_config.json
+
+#  ./test.sh mbpp 1 default_config_4_mini.json # done ektos apo  missing
+
+# ./test.sh mbpp 2 default_config.json
+
+./test.sh mbpp 1 default_config_llama.json
--- test.sh
+++ test.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 # Check if both arguments are provided
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 <dataset> <time>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Usage: $0 <dataset> <time> <config file>"
     exit 1
 fi
 
 # Get the input arguments as variables
 dataset=$1
 time=$2
-INPUT="${dataset}_${time}"
 
+CONFIG=$3
+
+#if config contains the word llamathen make variable model llama
+if [[ $CONFIG == *"llama"* ]]; then
+    model="llama"
+#if it contains the word bert then make variable model bert
+elif [[ $CONFIG == *"4_mini"* ]]; then
+    model="4_mini"
+elif [[ $CONFIG == *"4o"* ]]; then
+    model="4o"
+else 
+    model="3point5"
+fi
+INPUT="${dataset}_${model}_${time}"
 # Define the base path for the data and log directories
 DATA_DIR="/home/jim/HoarePrompt-data/PilotData/data"
-LOG_DIR="/home/jim/HoarePrompt-data/Results/$INPUT"
+LOG_DIR="/home/jim/HoarePrompt-data/Results/Pilot_reruns/$INPUT"
 
 # Create the log directory if it doesn't exist
 mkdir -p "$LOG_DIR"
@@ -27,7 +40,7 @@ echo "Processing ${#data_files[@]} data files..."
 # Loop over each data file and run the command
 for data_file in "${data_files[@]}"; do
     echo "Processing $data_file..."
-    python3 -m src.main --data "$data_file" --log "$LOG_DIR"
+    python3 -m src.main --config "$CONFIG" --data "$data_file" --log "$LOG_DIR"
     if [ $? -ne 0 ]; then
         echo "Error processing $data_file. Exiting."
         exit 1
