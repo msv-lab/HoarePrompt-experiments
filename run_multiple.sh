fb3f4205600413f456acff3a171a891f0a3b36e9
jimbou <jimbou1999@gmail.com>
2024-12-02 16:43:46 +0800

    prepared runs of new pilot datasets

--- run_multiple.sh
+++ run_multiple.sh
@@ -18,15 +18,27 @@
 # ./test.sh apps 4 default_config_4_mini.json
 # ./test.sh apps 5 default_config_4_mini.json
 
- ./test.sh mbpp 1 default_config_4_mini.json # done ektos apo  missing
+#  ./test.sh mbpp 1 default_config_4_mini.json # done ektos apo  missing
 
-./test.sh mbpp 1 default_config.json
+# ./test.sh mbpp 1 default_config.json
 
- ./test.sh mbpp 2 default_config_4_mini.json # done ektos apo  missing
+#  ./test.sh mbpp 2 default_config_4_mini.json # done ektos apo  missing
 
-./test.sh mbpp 2 default_config.json
- ./test.sh mbpp 3 default_config_4_mini.json # done ektos apo  missing
+# ./test.sh mbpp 2 default_config.json
+#  ./test.sh mbpp 3 default_config_4_mini.json # done ektos apo  missing
 
-./test.sh mbpp 3 default_config.json
+# ./test.sh mbpp 3 default_config.json
 
-# ./test.sh mbpp 1 default_config_llama.json
+# # ./test.sh mbpp 1 default_config_llama.json
+# ./test_fast.sh apps 1 default_config_4_mini.json
+# ./test_fast.sh apps 2 default_config_4_mini.json
+# ./test_fast.sh apps 3 default_config_4_mini.json
+# ./test_fast.sh apps 4 default_config_4_mini.json
+# ./test_fast.sh apps 5 default_config_4_mini.json
+
+./test_fast_75.sh mbpp 1 default_config_4_mini.json
+./test_fast_75.sh apps 1 default_config_4_mini.json
+# ./test_fast.sh mbpp 2 default_config.json
+# ./test_fast.sh mbpp 3 default_config.json
+# ./test_fast.sh mbpp 4 default_config.json
+# ./test_fast.sh mbpp 5 default_config.json
